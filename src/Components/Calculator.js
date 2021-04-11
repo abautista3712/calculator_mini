d526a73d607d43fe6c15730988f8a15fff6b34a7
Abelard Bautista <abelardbautista@gmail.com>
2021-04-10 22:43:50 -0700

    Moved components directory. Added performOperation function.

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import CalculatorKey from "./CalculatorKey";
 import "./Calculator.css";
 
-function Calculator() {
+const Calculator = () => {
   const [previousValue, setPreviousValue] = useState(null);
   const [nextValue, setNextValue] = useState("0");
   const [operation, setOperation] = useState(null);
 
-  useEffect(() => {}, [op, nextValue, previousValue]);
+  useEffect(() => {}, [operation, nextValue, previousValue]);
 
   const CalculatorOperations = {
     "*": (x, y) => x * y,
@@ -17,26 +17,39 @@ function Calculator() {
     "=": (x, y) => y,
   };
 
+  const performOperation = () => {
+    let tmp = CalculatorOperations[operation](
+      parseFloat(previousValue),
+      parseFloat(nextValue)
+    );
+    setOperation(null);
+    setNextValue(String(tmp));
+    setPreviousValue(null);
+  };
+
+  const handleOperation = (value) => {};
+
   return (
     <div className="calculator">
       <div className="calculator-input">
-        <div className="result">{results}</div>
+        <div className="result"></div>
+        {/* <div className="result">{results}</div> */}
       </div>
       <div className="calculator-keypad">
         <div className="keys-function">
-          <CalculatorKey keyValue={"c"} onClick={handleOperation} />
+          {/* <CalculatorKey keyValue={"c"} onClick={handleOperation} />
           <CalculatorKey keyValue={"\xB1"} onClick={handleOperation} />
-          <CalculatorKey keyValue={"%"} onClick={handleOperation} />
+          <CalculatorKey keyValue={"%"} onClick={handleOperation} /> */}
         </div>
         <div className="keys-operators">
-          <CalculatorKey keyValue={"+"} onClick={handleOperation} />
+          {/* <CalculatorKey keyValue={"+"} onClick={handleOperation} />
           <CalculatorKey keyValue={"-"} onClick={handleOperation} />
           <CalculatorKey keyValue={"*"} onClick={handleOperation} />
           <CalculatorKey keyValue={"/"} onClick={handleOperation} />
-          <CalculatorKey keyValue={"="} onClick={handleOperation} />
+          <CalculatorKey keyValue={"="} onClick={handleOperation} /> */}
         </div>
         <div className="keys-numbers">
-          <CalculatorKey keyValue={9} onClick={handleOperation} />
+          {/* <CalculatorKey keyValue={9} onClick={handleOperation} />
           <CalculatorKey keyValue={8} onClick={handleOperation} />
           <CalculatorKey keyValue={7} onClick={handleOperation} />
           <CalculatorKey keyValue={6} onClick={handleOperation} />
@@ -54,11 +67,11 @@ function Calculator() {
             className="key-zero"
             keyValue={0}
             onClick={handleOperation}
-          />
+          /> */}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Calculator;
