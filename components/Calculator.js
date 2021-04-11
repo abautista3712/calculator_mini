b2b7cc545ba8124a5bcaadc15768dfa3f8967241
Abelard Bautista <abelardbautista@gmail.com>
2021-04-10 22:06:37 -0700

    Added calculator keypad foundation.

---
+++ components/Calculator.js
@@ -0,0 +1,50 @@
+import React from "react";
+import CalculatorKey from "./CalculatorKey";
+import "./Calculator.css";
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="calculator-input">
+        <div className="result">{results}</div>
+      </div>
+      <div className="calculator-keypad">
+        <div className="keys-function">
+          <CalculatorKey keyValue={"c"} onClick={handleOperation} />
+          <CalculatorKey keyValue={"\xB1"} onClick={handleOperation} />
+          <CalculatorKey keyValue={"%"} onClick={handleOperation} />
+        </div>
+        <div className="keys-operators">
+          <CalculatorKey keyValue={"+"} onClick={handleOperation} />
+          <CalculatorKey keyValue={"-"} onClick={handleOperation} />
+          <CalculatorKey keyValue={"*"} onClick={handleOperation} />
+          <CalculatorKey keyValue={"/"} onClick={handleOperation} />
+          <CalculatorKey keyValue={"="} onClick={handleOperation} />
+        </div>
+        <div className="keys-numbers">
+          <CalculatorKey keyValue={9} onClick={handleOperation} />
+          <CalculatorKey keyValue={8} onClick={handleOperation} />
+          <CalculatorKey keyValue={7} onClick={handleOperation} />
+          <CalculatorKey keyValue={6} onClick={handleOperation} />
+          <CalculatorKey keyValue={5} onClick={handleOperation} />
+          <CalculatorKey keyValue={4} onClick={handleOperation} />
+          <CalculatorKey keyValue={3} onClick={handleOperation} />
+          <CalculatorKey keyValue={2} onClick={handleOperation} />
+          <CalculatorKey keyValue={1} onClick={handleOperation} />
+          <CalculatorKey
+            className="key-dot"
+            keyValue={"."}
+            onClick={handleOperation}
+          />
+          <CalculatorKey
+            className="key-zero"
+            keyValue={0}
+            onClick={handleOperation}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
